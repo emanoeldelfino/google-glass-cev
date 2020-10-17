db101cbff1441fb527bc56945464463a264bd00c
Emanoel <emanoelsilva1111@gmail.com>
2020-10-17 13:12:48 -0300

    Add google-glass project

---
+++ google-glass/javascript/funcoes.js
@@ -0,0 +1,7 @@
+function mudaFoto(foto) {
+    document.getElementById("icone").src = foto;
+}
+
+function changeIframe() {
+    document.getElementById("frame-spec").src = "google-glass.html#topo";
+}
